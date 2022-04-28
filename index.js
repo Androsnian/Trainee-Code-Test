cc3a25f261f0159293d9e319013a1e77f041ab03
Martin Höglund <83273759+Androsnian@users.noreply.github.com>
2022-04-28 13:50:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const moviesJson = require('./movies.json');
 class MovieAPI {
 	constructor(movies) {
-		this.movies = movies;
+		this.movies = JSON.parse(movies);
 	}
 
 	fetchAllMovies() {
@@ -9,13 +9,9 @@ class MovieAPI {
 		return this.movies;
 	}
 
-	//1. (REQUIRED) When instantiating the class with the imported movies.json file,
-	// add an “id” and a random “rating” from 1 to 5 for each movie before storing it.
 	moviePrep() {
-		var id = 0;
-		for(var i = 0; i < this.movies.length; i++) {
-			movie.id = id;
-			id++;
+		for (var i = 0; i < this.movies.length; i++) {
+			movie.id = i;
 			movie.rating = movieRating();
 		}
 	}
@@ -24,18 +20,17 @@ class MovieAPI {
 		return Math.floor(Math.random() * 5) + 1;
 	}
 
-	//2. A method that returns movies from a certain genre.
 	fetchMoviesFromGenre() {
 		var genre = prompt("Please enter a genre to search for");
 		var moviesOfGenre = [];
-		for(var i = 0; i < allMovies.length; i++) {
-			if(allMovies[i].genre === genre) {
-				moviesOfGenre.push(movies[i]);
-			}
-		}
 		if (!genre) {
 			console.error("You need to enter a genre");
 			return moviesOfGenre;
+		}
+		for (var i = 0; i < allMovies.length; i++) {
+			if (allMovies[i].genre === genre) {
+				moviesOfGenre.push(movies[i]);
+			}
 		}
 		if (moviesOfGenre.length === 0) {
 			console.error("No movies with a genre of " + genre + " was found.");
@@ -43,22 +38,17 @@ class MovieAPI {
 		return moviesOfGenre;
 	}
 
-	//3. A method that removes a movie with a certain id (if found).
-	removeMovie() {
-		var id = prompt("Please enter an id to be removed");
+	removeMovie(id) {
 		allMovies = allMovies.filter(movie => movie.id == id);
 	}
 
-	//4. A method that returns the movies with the subtitle and thumb properties filtered out.
 	filterSubtitleThumb() {
 		var filteredMovies = allMovies;
-		delete filteredMovies['subtitle'];
+		delete filteredMovies.subtitle;
 		delete filteredMovies.thumb;
 		return filteredMovies;
 	}
 
-	//9. A method that allows the user to add a new movie object to the movie list (supply all properties but the “id” and “rating”. 
-	//The “id” and “rating” properties should be added internally by the method.
 	addMovie() {
 		var description = prompt("Enter a description for the movie:");
 		var sources = prompt("Enter a source for the movie");
@@ -68,32 +58,32 @@ class MovieAPI {
 		var genre = prompt("Enter a genre for the movie");
 		var id = allMovies.length + 1;
 		var rating = movieRating();
-		allMovies.push({"description":description,"sources":sources,"subtitle":subtitle,"thumb":thumb,"title":title,
-		"genre":genre,"id":id,"rating":rating});
+		allMovies.push({
+			"description": description, "sources": sources, "subtitle": subtitle, "thumb": thumb, "title": title,
+			"genre": genre, "id": id, "rating": rating
+		});
 	}
 
-	//10. A method that returns a movie with a certain id (if found).
 	findIdMovie(id) {
-		for(var i; i < allMovies.length; i++) {
-			if(allMovies[i].id === id) {
+		for (var i; i < allMovies.length; i++) {
+			if (allMovies[i].id === id) {
 				return allMovies[i];
 			}
 		}
 		console.log("No movie with " + id + " was found.");
 	}
 
-	//11. A method that changes the title of a movie with a certain id (if found). 
-	//The updated title should be sent in as an argument to the method.
-	changeNameOfIdMovie(newTitle) {
+	changeTitleOfIdMovie(newTitle) {
 		var id = prompt("Please enter id of movie's title to be changed:");
-		for(var i; i < allMovies.length; i++) {
-			if(allMovies[i].id === id) {
+		for (var i; i < allMovies.length; i++) {
+			if (allMovies[i].id === id) {
 				allMovies[i].title = newTitle;
 				return;
 			}
 		}
+		console.log("No movie with " + id + " was found, no title was changed.");
 	}
 }
 
 const API = new MovieAPI(moviesJson);
-const allMovies = JSON.parse(API.fetchAllMovies());
+const allMovies = API.fetchAllMovies();
